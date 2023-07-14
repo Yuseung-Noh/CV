17adfcea13729e0662cef3fb3ca0e3a314d7ca7f
Yuseung <44202594+Yuseung-Noh@users.noreply.github.com>
2023-07-14 10:27:42 +0900

    Add btn

--- res/js/btn_lang.js
+++ res/js/btn_lang.js
@@ -1,9 +1,9 @@
 window.onload = function () {
     // 버튼 이벤트
     document.getElementById("btn-en").addEventListener("click", e => {
-        window.location.replace("../../en/index.html")
+        window.location.replace("../en/index.html")
     })
     document.getElementById("btn-ko").addEventListener("click", e => {
-        window.location.replace("../../ko/index.html")
+        window.location.replace("../ko/index.html")
     })
 }
